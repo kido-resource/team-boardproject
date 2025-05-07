05dd51c44a2c03d44a9f81c4f07d33dd3d077465
LSHmain <qhrqn98@daum.net>
2025-05-08 08:18:16 +0900

    add: 이상현 작업본입니다.

---
+++ src/com/shinhan/common/BoardView.java
@@ -0,0 +1,27 @@
+package com.shinhan.common;
+
+import java.util.List;
+
+public class BoardView {
+	public static void display(String message) {
+		System.out.println(message);
+	}
+	
+	public static void display(List<BoardDTO> result) {
+		if (result == null || result.isEmpty()) {
+			System.out.println("");
+			System.out.println("해당하는 글번호가 존재하지 않습니다.");
+			System.out.println("");
+		}
+		System.out.println("");
+		System.out.println("======== 글 조회 ========");
+		for (BoardDTO e : result) {
+            System.out.println("작성자 : "+e.getNickname());
+            System.out.println("작성일 : "+e.getCreate_date());
+            System.out.println("수정일 : "+e.getUpdate_date());
+            System.out.println("제목 : "+e.getTitle());
+            System.out.println("내용 : "+e.getCommnet());
+            System.out.println("");
+        }
+	}
+}
--- src/com/shinhan/common/ControllerFactory.java
+++ src/com/shinhan/common/ControllerFactory.java
@@ -1,6 +1,14 @@
 package com.shinhan.common;
 
-// Factory pattern
 public class ControllerFactory {
-
+	public static CommonControllerInterface make(String business) {
+		CommonControllerInterface controller = null;
+		switch(business) {
+		case "insert" -> {controller = new InsertController();}
+		case "select" -> {controller = new SelectController();}
+//		case "update" -> {controller = new UpdateController();}
+//		case "delete" -> {controller = new DeleteController();}
+		}
+		return controller;
+	}
 }
